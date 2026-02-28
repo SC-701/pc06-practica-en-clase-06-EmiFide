4e7d88e5fba6afd602fae50f32bc66d909e4af60
Emily094 <ecortes80241@ufide.ac.cr>
2026-02-28 08:51:54 -0600

    Revert "commit inicial"
    This reverts commit 9d7dfbbc221d2e8b79013b43b6ae320baeffa522.

--- Vehiculo.API/Abstracciones/Interfaces/Servicios/IRegistroServicio.cs
+++
@@ -1,9 +0,0 @@
-﻿using Abstracciones.Modelos.Servicios.Registro;
-
-namespace Abstracciones.Interfaces.Servicios
-{
-    public interface IRegistroServicio
-    {
-        Task<Propietario> Obtener(string placa);
-    }
-}
