0bc30ab1ee587821da3308b5e61d200a37fede8e
Emily094 <ecortes80241@ufide.ac.cr>
2026-03-02 19:09:12 -0600

    ajustes en string de conexión

--- Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
+++ Vehiculo.API/BD/dbo/Stored Procedures/AgregarVehiculo.sql
@@ -10,7 +10,7 @@
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
-	-- interfering with SELECT statements.
+	-- interfering with SELECT statement.
 	SET NOCOUNT ON;
 
     -- Insert statements for procedure here - prueba práctica de semana 6
